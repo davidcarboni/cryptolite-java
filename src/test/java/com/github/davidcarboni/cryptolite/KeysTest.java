9fe17867cd3b87528195f79b299cc74503545d2a
David Carboni <david@carboni.io>
2018-04-15 11:43:54 +0100

    Test for deterministic key generation.

--- src/test/java/com/github/davidcarboni/cryptolite/KeysTest.java
+++ src/test/java/com/github/davidcarboni/cryptolite/KeysTest.java
@@ -6,6 +6,7 @@ import javax.crypto.SecretKey;
 import java.security.KeyPair;
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
@@ -66,5 +67,29 @@ public class KeysTest {
         assertFalse(Arrays.equals(keyPair1.getPrivate().getEncoded(), keyPair2.getPrivate().getEncoded()));
         assertFalse(Arrays.equals(keyPair1.getPublic().getEncoded(), keyPair2.getPublic().getEncoded()));
     }
+
+    /**
+     * Test method for {@link com.github.davidcarboni.cryptolite.Keys#generateSecretKey(String, String)}.
+     * <p>
+     * Tests a known password and salt value to make sure the expected key is generated.
+     */
+    @Test
+    public void testGenerateSecretKey() {
+
+        // Given
+        // A known password/salt -> key vector
+        String password = "Mary had a little Café";
+        String salt = "EvwdaavC8dRvR4RPaI9Gkg==";
+        String keyHex = "e73d452399476f0488b32b0bea2b8c0da35c33b122cd52c6ed35188e4117f448";
+
+        // When
+        // We generate the key
+        SecretKey key = Keys.generateSecretKey(password, salt);
+
+        // Then
+        // We should get the expected key
+        assertEquals(keyHex, ByteArray.toHex(key.getEncoded()));
+    }
+
 
 }
